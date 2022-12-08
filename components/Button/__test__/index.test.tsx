66040d948345c87d91890afbbc6e110cdb98864b
wooram <zzzwwwrrr66@gmail.com>
2022-12-08 19:44:15 +0900

    <add>

--- components/Button/__test__/index.test.tsx
+++ components/Button/__test__/index.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { mount } from "enzyme";
-import Home from "../../pages/index";
+import Home from "../../../pages/index";
+
 describe("Pages", () => {
   describe("Home", () => {
     it("should render without throwing an error", function () {
